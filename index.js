7f710b814b9928981d0603a2f46f604f1d5cc483
LoopFruits <Kensworldc@gmail.com>
2022-02-07 17:24:45 -0500

    Completed assignment with notes

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Your code goes here
+document.addEventListener("DOMContentLoaded", function(){
+    console.log('The DOM has loaded');
+const contentLoaded = document.getElementById('text');
+contentLoaded.innerHTML = 'This is really cool!';    
+})
+
+// no going to like this doesn't feel correct. Need to take a better look 
+
+// Another way to get the code working. 
+// document.addEventListener("DOMContentLoaded", function() {  
+//     console.log("This is really cool!");
+// })
+
+// function textFix(text) {
+//     const text = document.getElementById('text');
+// }
